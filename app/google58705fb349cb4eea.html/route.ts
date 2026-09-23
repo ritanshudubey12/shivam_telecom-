ab63b22d59a7d0de27daad6d42cc9de3af4025d6
Ritanshudubey <ritanshudubey81@gmail.com>
2026-09-23 15:48:05 +0530

    chore: remove accidental route from app directory

--- app/google58705fb349cb4eea.html/route.ts
+++
@@ -1,8 +0,0 @@
-export function GET() {
-  return new Response("google-site-verification: google58705fb349cb4eea.html", {
-    status: 200,
-    headers: {
-      "Content-Type": "text/html; charset=utf-8",
-    },
-  });
-}
